1d67b7cdcfe947977f9ce171ae01cc1e80a6bee7
Isidora Milovanov <isidoramilovanov@gmail.com>
2018-04-23 20:47:54 +0200

    changes

---
+++ src/entities/User.js
@@ -0,0 +1,15 @@
+
+class User {
+    constructor(user){
+        this.photo = user.picture.large;
+        this.firstName = user.name.first;
+        this.lastName = user.name.last;
+        this.email = user.email;
+        this.birthday = user.dob;
+        this.gender = user.gender;
+    }
+
+    
+}
+
+export default User
